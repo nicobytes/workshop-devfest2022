9a16a6406408aa8caff362360896150695a5241c
Nicolas Molina Monroy <hi@nicobytes.com>
2022-11-19 12:11:49 -0400

    fix linter

--- src/app/home/components/contact/contact.component.ts
+++ src/app/home/components/contact/contact.component.ts
@@ -1,17 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'contact',
+  selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
 })
-export class Contact implements OnInit {
-
-  last_name = '';
+export class ContactComponent  {
 
   constructor() {}
 
-  ngOnInit(): void {
-
-  }
 }
